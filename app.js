e9752dd4e0e9d6301bf2e95b4307721cbff399ed
mishel254 <sonimichie@gmail.com>
2020-09-06 20:06:27 +0300

    prizing the pizzas

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 $('#order').click( (e)=>{
     $('#form').css({'display': 'block'})
 })
+var pizzaPrice;
+var nm = $('input#pizzaNumber').val();
+var numberOfPizza  =nm
 
+//var deliveryFee;
 //pizza prices
-var extra = $('#xx-large') ;
+/* var extra = $('#xx-large') ;
 extra = 1400
 var large = $('#large');
 large = 1100;
@@ -12,8 +16,7 @@ meduim = 1800;
 var small = $('#small');
 small = 600;
 
-var deliveryfee = 250;
-
+//toppings prices
 var sausage = $('#Sausage') 
 sausage = 200
 var cheese = $('#extra-cheese') 
@@ -23,51 +26,167 @@ onions = 150
 var mushrooms = $('#mushrooms');
 mushrooms = 400
 var pepper = $('#green pepper');
-pepper = 100  
+pepper = 100  */ 
 
-console.log(extra)
-//all spaces filled
-function allcategorieschecked(){
-    if($('.pizzaSize').prop('checked') == false){
-        alert('fill all spaces')
-    }else if($('.pizzaCrust').prop('checked') == false){
-        alert('fill crust')
-    }else ( $('.pizzaToppings').prop('checked') == false)
-    {
-        alert('select toppings')
+//all check
+function validation(){
+   
+    if($('.pizzaToppings:checked').length == 0){
+        alert('select atleast one checkbox  ')
+        
     }
+   
 }
 
-function sausageToppings(){
-    if($(xx-large).prop('checked') == true){
-        var pizzaPrice = extra * sausage
-    }else if($('.large').prop('checked')){
-        var pizzaPrice = large * sausage
-    }else if($('.medium').prop('checked')){
-        var pizzaPrice = medium * sausage
+
+/* function pizzasize(){
+    if($('#xx-large').prop('checked')== true){
+        if($('#sausage').prop('checked') == true){
+            pizzaPrice = extra + sausage
+        }else if($('#extra-chesee').prop('checked') == true){
+            pizzaPrice = extra + cheese
+        }else if($('#mushroom').prop('checked') == true){
+            pizzaPrice = extra + mushroom
+        }else if($('#onions').prop('checked') == true){
+            pizzaPrice = extra + onions
+        }else 
+            pizzaPrice =  extra + pepper
+
+            large
+    }else if ($('#large').prop('checked') == true){
+        if($('#sausage').prop('checked') == true){
+            pizzaPrice = large + sausage
+        }else if($('#extra-cheese').prop('checked') == true){
+            pizzaPrice = large + cheese
+        }else if($('#mushroom').prop('checked') == true){
+            pizzaPrice = large + mushroom
+        }else if($('#onions').prop('checked') == true){
+            pizzaPrice = large + onions
+        }else 
+            pizzaPrice =  large + pepper
+            //medium
+    }else if ($('#small').prop('checked') == true){
+        if($('#sausage').prop('checked') == true){
+            pizzaPrice = medium + sausage
+        }else if($('#extra-cheese').prop('checked') == true){
+            pizzaPrice = medium + cheese
+        }else if($('#mushroom').prop('checked') == true){
+            pizzaPrice = medium + mushroom
+        }else if($('#onions').prop('checked') == true){
+            pizzaPrice = medium + onions
+        }else 
+            pizzaPrice =  medium + pepper
     }else
-        var pizzaPrice = small * sausage
-}
+        //small pizza
+        if($('#sausage').prop('checked') == true){
+            pizzaPrice = small + sausage
+        }else if($('#extra-cheese').prop('checked') == true){
+            pizzaPrice = small + cheese
+        }else if($('#mushroom').prop('checked') == true){
+            pizzaPrice = small + mushroom
+        }else if($('#onions').prop('checked') == true){
+            pizzaPrice = small + onions
+        }else 
+            {pizzaPrice =  small + pepper}
+          
+} */
 
-
-function pizzasize(){
-    if($('.extra').prop('checked')== true){
-        if($('.sausage').prop('checked') == true){
-            var pizzaPrice = extra * sausage
-        }else if ($('.chesse').prop('checked') == true){
-            var pizzaPrice = extra * chesee
-        }
-    }
-}
-$('#submit').click( (e)=>{
-     allcategorieschecked();
-     //sausage topping
-
+//checking for a delivery fee
+/* function deliveryFee(){
+    var deliveryfee = 200; 
     var delivery = $('#delivery') 
     if(delivery.prop('checked') == true){
        var address = prompt('Enter your Address');
     }
+} */
 
+$('#submit').click( (e)=>{
+    e.preventDefault();
+    validation();
+        var deliveryfee = 200; 
+        var delivery = $('#delivery') 
+        if(delivery.prop('checked') == true){
+           var address = prompt('Enter your Address');
+        }
+
+        //
+        var extra = $('#xx-large') ;
+extra = 1400
+var large = $('#large');
+large = 1100;
+var medium = $('#medium');
+meduim = 1800;
+var small = $('#small');
+small = 600;
+
+//toppings prices
+var sausage = $('#Sausage') 
+sausage = 200
+var cheese = $('#extra-cheese') 
+cheese = 300;
+var onions = $('#onions') 
+onions = 150
+var mushrooms = $('#mushrooms');
+mushrooms = 400
+var pepper = $('#green pepper');
+pepper = 100 
+        //
+    function pizzasize(){
+        if($('#xx-large').prop('checked')== true){
+            if($('#sausage').prop('checked') == true){
+                pizzaPrice = extra + sausage
+            }else if($('#extra-chesee').prop('checked') == true){
+                pizzaPrice = extra + cheese
+            }else if($('#mushroom').prop('checked') == true){
+                pizzaPrice = extra + mushroom
+            }else if($('#onions').prop('checked') == true){
+                pizzaPrice = extra + onions
+            }else 
+                pizzaPrice =  extra + pepper
     
-})
+                large
+        }else if ($('#large').prop('checked') == true){
+            if($('#sausage').prop('checked') == true){
+                pizzaPrice = large + sausage
+            }else if($('#extra-cheese').prop('checked') == true){
+                pizzaPrice = large + cheese
+            }else if($('#mushroom').prop('checked') == true){
+                pizzaPrice = large + mushroom
+            }else if($('#onions').prop('checked') == true){
+                pizzaPrice = large + onions
+            }else 
+                pizzaPrice =  large + pepper
+                //medium
+        }else if ($('#small').prop('checked') == true){
+            if($('#sausage').prop('checked') == true){
+                pizzaPrice = medium + sausage
+            }else if($('#extra-cheese').prop('checked') == true){
+                pizzaPrice = medium + cheese
+            }else if($('#mushroom').prop('checked') == true){
+                pizzaPrice = medium + mushroom
+            }else if($('#onions').prop('checked') == true){
+                pizzaPrice = medium + onions
+            }else 
+                pizzaPrice =  medium + pepper
+        }else
+            //small pizza
+            if($('#sausage').prop('checked') == true){
+                alert()
+                pizzaPrice = small + sausage
+            }else if($('#extra-cheese').prop('checked').val() == true){
+                console.log(pizzaPrice = small + cheese)
+            }else if($('#mushroom').prop('checked') == true){
+                pizzaPrice = small + mushroom
+            }else if($('#onions').prop('checked') == true){
+                pizzaPrice = small + onions
+            }else 
+                pizzaPrice =  small + pepper
+              
+                console.log( pizzaPrice)
+    }
 
+    pizzasize()
+    console.log(large + pepper + deliveryfee)
+            console.log(  deliveryfee)
+  
+})
